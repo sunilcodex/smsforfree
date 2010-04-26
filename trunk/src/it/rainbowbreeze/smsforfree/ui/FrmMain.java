4cd3239ff34733069925c3da3f72a4565c0ff318
alfredo.morresi <alfredo.morresi@01005c77-cac7-e1af-68a0-f272ea10e872>
2010-04-26 07:46:15 +0000

    added docs and FrmMain

---
+++ trunk/src/it/rainbowbreeze/smsforfree/ui/FrmMain.java
@@ -0,0 +1,18 @@
+package it.rainbowbreeze.smsforfree.ui;
+
+import it.rainbowbreeze.smsforfree.R;
+import it.rainbowbreeze.smsforfree.logic.AimonApiManager;
+import it.rainbowbreeze.smsforfree.logic.AimonDictionary;
+import it.rainbowbreeze.smsforfree.logic.JacksmsApiManager;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrmMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+}
