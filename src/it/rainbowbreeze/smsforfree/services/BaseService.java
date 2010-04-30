f3c2c96005587b7d5057faac7f2618cd91a1c202
alfredo.morresi <alfredo.morresi@01005c77-cac7-e1af-68a0-f272ea10e872>
2010-04-30 07:14:38 +0000

    base skeleton for jacksms

--- src/it/rainbowbreeze/smsforfree/services/BaseService.java
+++ src/it/rainbowbreeze/smsforfree/services/BaseService.java
@@ -1,15 +1,41 @@
 package it.rainbowbreeze.smsforfree.services;
 
+import java.util.List;
+
 import android.text.TextUtils;
 
+
+/**
+ * Base class for sms service management
+ * 
+ *  Each service could have more "configurations" (for example, multi-provider service)
+ * 
+ * 
+ * @author Alfredo "Rainbowbreeze" Morresi
+ *
+ */
 public abstract class BaseService
 {
-	//---------- Ctors
-
 	//---------- Private fields
 
+	
+	
+	
 	//---------- Public properties
+	
+	/**
+	 * The service name
+	 */
+	public abstract String getServiceName();
 
+	/**
+	 * Has this service configurations?
+	 */
+	public abstract boolean hasConfigurations();
+	
+
+	
+	
 	//---------- Public fields
 	public static final String ERROR_NO_USERNAME = "NO_USERNAME";
 	public static final String ERROR_NO_PASSWORD = "NO_PASSWORD";
@@ -17,9 +43,37 @@ public abstract class BaseService
 	public static final String ERROR_NO_REPLY_FROM_SITE = "NO_REPLY";
 	
 	
-	
+
+
 	//---------- Public methods
-	protected void checkIfCredentialsAreEmpty(String username, String password)
+	
+	public abstract List<String> getServiceParametersDesc();
+
+	public abstract List<String> getServiceParameters();
+
+	public abstract void setServiceParameters(List<String> parameters);
+	
+	public abstract List<String> getAllConfigurationsName();
+
+	public abstract List<String> getConfiguredConfigurationsName();
+
+	public abstract List<String> getConfigurationParametersDesc(String configurationId);
+
+	public abstract List<String> setConfigurationParameters(String configurationId, List<String> parameters);
+
+	public abstract List<String> getConfigurationParameters(String configurationId);
+
+	
+	//---------- Private methods
+	
+	/**
+	 * Checks if username and password are not empty
+	 * Throws an error if one of the two are not correct
+	 * 
+	 * @param username
+	 * @param password
+	 */
+	protected void checkCredentialsValidity(String username, String password)
 		throws IllegalArgumentException
 	{
     	if (TextUtils.isEmpty(username))
@@ -27,7 +81,5 @@ public abstract class BaseService
     	if (TextUtils.isEmpty(password))
     		throw new IllegalArgumentException(ERROR_NO_PASSWORD);
 	}
-
-	//---------- Private methods
 
 }
--- src/it/rainbowbreeze/smsforfree/ui/FrmMain.java
+++ src/it/rainbowbreeze/smsforfree/ui/FrmMain.java
@@ -3,6 +3,7 @@ package it.rainbowbreeze.smsforfree.ui;
 import it.rainbowbreeze.smsforfree.R;
 import it.rainbowbreeze.smsforfree.services.AimonDictionary;
 import it.rainbowbreeze.smsforfree.services.AimonService;
+import it.rainbowbreeze.smsforfree.services.JacksmsService;
 import it.rainbowbreeze.smsforfree.services.OldJacksmsService;
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,6 +14,11 @@ public class FrmMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        JacksmsService.instance().loadTemplateServices();
+        JacksmsService.instance().loadUserService();
+        JacksmsService.instance().loadCredentials();
+        JacksmsService.instance().sendSms(62, "+393927686894", "Ciao da me che sono il re!");
         
     }
 }
