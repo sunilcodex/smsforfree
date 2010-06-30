77497bd8fd87cac2bfcc86a7a2ad9a82cc3ac830
alfredo.morresi <alfredo.morresi@01005c77-cac7-e1af-68a0-f272ea10e872>
2010-06-30 06:48:38 +0000

    added test skeleton

---
+++ smsforfree_core_test/src/it/rainbowbreeze/smsforfree/data/WebserviceClientTest.java
@@ -0,0 +1,13 @@
+package it.rainbowbreeze.smsforfree.data;
+
+import android.test.AndroidTestCase;
+
+public class WebserviceClientTest
+	extends AndroidTestCase
+{
+	
+	public void testSimpleGet() {
+		assertTrue(true);
+	}
+
+}
