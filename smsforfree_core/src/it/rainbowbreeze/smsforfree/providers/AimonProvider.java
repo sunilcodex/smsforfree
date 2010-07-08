1668ddf11720209f13c7d833e6bc208cd60ab194
alfredo.morresi <alfredo.morresi@01005c77-cac7-e1af-68a0-f272ea10e872>
2010-07-08 07:08:47 +0000

    added license information to source file

--- smsforfree_core/src/it/rainbowbreeze/smsforfree/providers/AimonProvider.java
+++ smsforfree_core/src/it/rainbowbreeze/smsforfree/providers/AimonProvider.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2010 Alfredo Morresi
+ * 
+ * This file is part of SmsForFree project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.rainbowbreeze.smsforfree.providers;
 
 import java.util.ArrayList;
@@ -19,7 +38,6 @@ import it.rainbowbreeze.smsforfree.domain.SmsService;
 import it.rainbowbreeze.smsforfree.domain.SmsServiceCommand;
 import it.rainbowbreeze.smsforfree.domain.SmsServiceParameter;
 import it.rainbowbreeze.smsforfree.util.Base64;
-import it.rainbowbreeze.smsforfree.util.ParserUtils;
 
 /**
  * 
