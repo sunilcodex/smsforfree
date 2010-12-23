8cf25e0b31997a3dec6091e97d11e269bbaa95d6
alfredo.morresi <alfredo.morresi@01005c77-cac7-e1af-68a0-f272ea10e872>
2010-12-23 19:21:03 +0000

    point test project to right package

--- smsforfree_core_test/src/it/rainbowbreeze/smsforfree/common/Def.java
+++ smsforfree_core_test/src/it/rainbowbreeze/smsforfree/common/Def.java
@@ -30,17 +30,17 @@ public interface Def
 	public String TEST_DESTINATION = "XXXX";
 
 	public String AIMON_USERNAME = "f.martinelli@aimon.it";
-	public String AIMON_PASSWORD = "XXXX";
+	public String AIMON_PASSWORD = "smsf0rfr33";
 	public String AIMON_SENDER = "3912345678";
 	
 	public String SUBITOSMS_USERNAME = "fmartinelli";
-	public String SUBITOSMS_PASSWORD = "XXXX";
+	public String SUBITOSMS_PASSWORD = "outtura";
 	public String SUBITOSMS_SENDER = "3912345678";
 	
 	public String JACKSMS_USERNAME = "f.martinelli";
-	public String JACKSMS_PASSWORD = "XXXX";
+	public String JACKSMS_PASSWORD = "smsf0rfr33";
 	
 	public String VOIPSTUNT_USERNAME = "f.martinelli";
-	public String VOIPSTUNT_PASSWORD = "XXXX";
+	public String VOIPSTUNT_PASSWORD = "smsf0rfr33";
 	public String VOIPSTUNT_SENDER = "3912345678";
 }
