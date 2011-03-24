d1a1bad9cf46ebef020df6115fd96e051e4d4b05
sa.guarda <sa.guarda@01005c77-cac7-e1af-68a0-f272ea10e872>
2011-03-24 16:05:03 +0000

    - Modifiche per JackSms 3.0

--- branches/jacksms/smsforfree_core/src/it/rainbowbreeze/smsforfree/common/AppEnv.java
+++ branches/jacksms/smsforfree_core/src/it/rainbowbreeze/smsforfree/common/AppEnv.java
@@ -73,10 +73,10 @@ public class AppEnv
 	public final static String APP_INTERNAL_NAME = "JackSms";
     
 	/** Application version for internal use (update, crash report etc) */
-	public final static String APP_INTERNAL_VERSION = "0.1";
+	public final static String APP_INTERNAL_VERSION = "0324a";
 
 	/** address where send log */
-	public final static String EMAIL_FOR_LOG = "sa.guarda@gmail.com";
+	public final static String EMAIL_FOR_LOG = "sa.guarda@gmail.com;jamiro.it@gmail.com";
 	
 	/** Tag to use in the log */
 	public final static String LOG_TAG = "JackSms";
